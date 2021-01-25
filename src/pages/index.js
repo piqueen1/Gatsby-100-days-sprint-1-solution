f24a5c534e672bfbe8c806ec11ec15dd8022f089
Finn Thye <piqueen@gmail.com>
2021-01-24 21:06:13 -0700

    Added dynamic routing through graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
+import {Link, graphql} from 'gatsby'
 
+export const query = graphql `
+  query MyQuery {
+    allContentfulCity {
+      edges {
+        node {
+          name
+          description
+          location {
+            lat
+            lon
+          }
+          gatsbyPath(filePath: "/city/{contentfulCity.name}")
+        }
+      }
+    }
+  }
+`
 
-
-
-export default function Home() {
+export default function Home({data}) {
   return(
     <div>
-      Hi I am the home page
+      <h1>
+        Hi I am the home page
+      </h1>
+
+      <ul>
+        {
+          data.allContentfulCity.edges.map(({node:city}) =>(
+            <li 
+            key={city.name}
+            >
+              <Link to={city.gatsbyPath}>{city.name}</Link>
+              {city.description} - {city.location.lat} - {city.location.lon}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
